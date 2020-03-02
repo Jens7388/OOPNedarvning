4f380c7646a1895b3428df78bcf2c7c5a0ba6419
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-03-02 13:12:17 +0100

    Added remaining classes

---
+++ OOPNedarvning/DocumentFileInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPNedarvning
+{
+    class DocumentFileInfo: CustomFileInfo
+    {
+        public DocumentFileInfo(string fileName, int fileSize, DateTime creationTime)
+            : base(fileName, fileSize, creationTime)
+        {
+
+        }
+    }
+}
---
+++ OOPNedarvning/VideoFileInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPNedarvning
+{
+    class VideoFileInfo: ImageFileInfo
+    {
+        private int duration;
+        public VideoFileInfo(string fileName, int fileSize, DateTime creationTime, int width, int height, int duration)
+            : base(fileName, fileSize, creationTime, width, height)
+        {
+            Duration = duration;
+        }
+
+        public int Duration
+        {
+            get
+            {
+                return duration;
+            }
+
+            set
+            {
+                duration = value;
+            }
+        }
+        public override string ToString()
+        {
+            return "Video:" + fileName;
+        }
+    }
+}
