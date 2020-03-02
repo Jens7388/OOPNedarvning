ea96c4f97d8e6355e8d4eadb2d7c32d46aa19189
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-03-02 10:51:59 +0100

    Add project files.

---
+++ OOPNedarvning/CustomFileInfo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace OOPNedarvning
+{
+    abstract class CustomFileInfo
+    {
+        protected string fileName;
+        protected int fileSize;
+        protected DateTime creationTime;
+
+        public string FileName
+        {
+            get
+            {
+                return fileName;
+            }
+
+            set
+            {
+                fileName = value;
+            }
+        }
+
+        public int FileSize
+        {
+            get
+            {
+                return fileSize;
+            }
+
+            set
+            {
+                fileSize = value;
+            }
+        }
+
+        public DateTime CreationTime
+        {
+            get
+            {
+                return creationTime;
+            }
+
+            set
+            {
+                creationTime = value;
+            }
+        }
+
+        protected CustomFileInfo(string fileName, int fileSize, DateTime creationTime) 
+        {
+            FileName = fileName;
+            FileSize = fileSize;
+            CreationTime = creationTime;
+        }
+        public (bool, string) ValidateFileSize(int fileSize)
+        {
+            if(fileSize > 45)
+            {
+                return (false, "Filen må ikke være på mere end 45 mb!");
+            }
+            if(fileSize == 0) 
+            {
+                return (false, "Filens størrelse er ugyldig!");
+            }
+            if
+                { }
+        }
+    }
+}
