fd1d7a8136964e13ec0546849953f9058f44b2ec
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-03-03 10:36:51 +0100

    Added Shape and circle classes

---
+++ OOPNedarvningFormer/Circle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPNedarvningFormer
+{
+    class Circle: Shape
+    {
+        private double radius;
+
+        public Circle(int x, int y, double radius) : base(x, y)
+        {
+            Radius = radius;
+        }
+
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+
+            set
+            {
+                (bool isValid, string errorMessage) validationResult = ValidateRadius(value);
+                if(!validationResult.isValid)
+                {
+                    throw new ArgumentException(validationResult.errorMessage);
+                }
+                if(value != radius) 
+                {
+                    radius = value;
+                }
+            }
+        }
+        public (bool, string) ValidateRadius(double radius) 
+        {
+            if(radius < 0.0) 
+            {
+                return (false, "Radius må ikke være på under 0.0!");
+            }
+            else
+            {
+                return (true, String.Empty);
+            }
+        }
+        public override double CalculateArea()
+        {
+            return Math.PI * (radius * radius);
+        }
+        public override double CalculateCircumference()
+        {
+            return 2 * radius * Math.PI;
+        }
+        public override string ToString()
+        {
+            return $"Position: ({x},{y}) Radius: {radius}";
+        }
+    }
+}
---
+++ OOPNedarvningFormer/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OOPNedarvningFormer
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
